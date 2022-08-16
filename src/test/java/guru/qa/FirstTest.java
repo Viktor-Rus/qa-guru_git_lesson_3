27218293250e0e694474211443302a156983ff7a
Viktor_Rus <vaurusov@mts.ru>
2022-08-16 14:22:41 +0300

    add first test

---
+++ src/test/java/guru/qa/FirstTest.java
@@ -0,0 +1,16 @@
+package guru.qa;
+
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+
+   @Test
+   void FirstTest(){
+      Assert.assertEquals(2, 3);
+   }
+
+
+}
